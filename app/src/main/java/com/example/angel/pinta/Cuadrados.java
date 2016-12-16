7507735fb5b3334480589e2e33dc1d8d15047b31
asr16 <angelvalor16@gmail.com>
2016-12-16 16:58:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/angel/pinta/Cuadrados.java
@@ -0,0 +1,28 @@
+package com.example.angel.pinta;
+
+import android.graphics.Paint;
+import android.graphics.RectF;
+
+public class Cuadrados {
+    private RectF rect;
+    private Paint paint;
+
+    public Cuadrados(int color,float grosor,Paint.Style estilo){
+        rect = new RectF();
+        paint = new Paint();
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(color);
+        paint.setStyle(estilo);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeWidth(grosor);
+    }
+
+    public RectF getRect(){
+        return rect;
+    }
+
+    public Paint getPaint(){
+        return paint;
+    }
+}
---
+++ app/src/main/java/com/example/angel/pinta/Lineas.java
@@ -0,0 +1,37 @@
+package com.example.angel.pinta;
+
+import android.graphics.Paint;
+import android.graphics.Path;
+
+
+public class Lineas extends Path {
+    private Paint paint;
+    private Path path;
+
+    public Lineas(int color, float grosor,Paint.Style estilo){
+        path = new Path();
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(color);
+        paint.setStyle(estilo);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeWidth(grosor);
+    }
+
+    public Paint getPaint(){
+        return paint;
+    }
+
+    public Path getPath(){
+        return path;
+    }
+
+    public void setPaint(Paint p){
+        paint = p;
+    }
+
+    public void setPath(Path p){
+        path = p;
+    }
+
+}
